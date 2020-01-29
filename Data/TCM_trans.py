b7765a941bc978ea2854a7348437f1946ddd0ef8
dodobill <37468493+dodobill@users.noreply.github.com>
2020-01-29 15:52:35 +0800

    Update TCM_trans.py

--- Data/TCM_trans.py
+++ Data/TCM_trans.py
@@ -1,10 +1,7 @@
 import os
-fn = '_data/train.txt'
-
-'''
-以下仅规范化一句话
-改进：模块化，将大段代码转化成函数
-'''
+import pickle
+fn = '_data/train2.txt'
+tofile = '_data/train2.pk'
 class Entry:
     def __init__(self,id,old_id,word,pos,parent_id,entity,relation='_'):
         self.old_id=old_id
@@ -15,15 +12,15 @@ class Entry:
         self.entity = entity
         self.relation = relation
     def __str__(self):
-        values = [str(self.id),self.word,self.pos,str(self.parent_id),self.entity,self.relation]
+        values = [str(self.id),str(self.old_id),self.word,self.pos,str(self.parent_id),self.entity,self.relation]
         return '\t'.join(values)
 
 def _readline(fn:str,code="utf-8")->str:
-    mode = rmode(fn)
-    with open(fn,mode) as fr:
+    #mode = rmode(fn)
+    with open(fn,"rb") as fr:
         for i in fr:
-            if mode=="rb":
-                i = i.decode(encoding=code)
+            #if mode=="rb":
+            i = i.decode(encoding=code)
             yield i
 
 def rmode(fn:str)->str:
@@ -32,37 +29,67 @@ def rmode(fn:str)->str:
     else:
         return 'rb'
 
-if __name__ == '__main__':
-    old_id=""
+def wpk(fn,tofn,code="utf-8"):
+    tofile = open(tofn,'wb')
+    old_id = ""
     id = -1
     word = ""
     pos = ""
     parent_id = ""
     entity = ""
     sentence = []
-    for line in _readline(fn):
-        if line.strip()=="":
+    for idx,line in enumerate(_readline(fn,code)):
+        if line.strip() == "":
+            sentence.append(Entry(id, old_id, word, pos, parent_id, entity))#将最后一个字加入
             for i in sentence:
-                if i.parent_id!='_':
+                if i.parent_id != '_':
                     try:
-                        tar = next(filter(lambda x:x.old_id==i.parent_id,sentence))
+                        tar = next(filter(lambda x: x.old_id == i.parent_id, sentence))
                     except:
                         print('没有找到对应序号的实例')
-                    i.parent_id=tar.id
-            print("完成第一句")
-            break
-    
-        items = line.strip().split('\t')#[item,...]
-        assert len(items)==6,items
-        if items[2]=="pr":
-            if id!=-1:
-                sentence.append(Entry(id,old_id,word,pos,parent_id,entity))
+                        print(idx)
+
+                    i.parent_id = tar.id
+            pickle.dump(sentence,tofile)
+            old_id = ""
+            id = -1
+            word = ""
+            pos = ""
+            parent_id = ""
+            entity = ""
+            sentence = []
+            continue
+
+        items = line.strip().split('\t')  # [item,...]
+        assert len(items) == 6, idx
+        if items[2] == "pr":
+            if id != -1:
+                sentence.append(Entry(id, old_id, word, pos, parent_id, entity))
             id += 1
-            old_id=items[0]
+            old_id = items[0]
             entity = items[3]
             word = items[1]
             pos = items[5]
             parent_id = items[4]
         else:
-            word+=items[1]
+            word += items[1]
+    tofile.close()
+def rpk(pk):
+    with open(pk,'rb') as fr:
+        while(True):
+            try:
+                data = pickle.load(fr)
+            except:
+                print('read over')
+                break
+            yield data
+
+
+if __name__ == '__main__':
+    pass
+
+
+
+
+
 
